e9cc0d2e7e7ee18117d063af16ec84b74867e810
bithitio <84578059+bithitio@users.noreply.github.com>
2021-05-23 17:43:46 +0700

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -33,7 +33,7 @@ const Navbar = () => {
                 <NavLink onClick={closeSidebar} to='/contact'>
                     Contact
                 </NavLink>
-                <NavLink onClick={closeSidebar} to='/github'>
+                <NavLink onClick={closeSidebar} exact to='/github'>
                     Github
                 </NavLink>
             </NavMenu>
