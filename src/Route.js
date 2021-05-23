73c27eec6a2c5e648c8ffeec06212eb4bb494737
bithitio <84578059+bithitio@users.noreply.github.com>
2021-05-23 18:47:00 +0700

    Update Route.js

--- src/Route.js
+++ src/Route.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, useHistory } from 'react-router-dom';
 import HomePage from './pages/Home';
+import HowtobuyPage from './pages/Howtobuy';
 import ContactPage from './pages/Contact';
 import IcoPage from './pages/Ico';
 
@@ -9,6 +10,7 @@ const Router = () => {
   return (
     <div>
       <Route path="/" exact component={HomePage} />
+      <Route path="/howtobuy" exact component={HowtobuyPage} />
       <Route path="/contact" exact component={ContactPage} />
       <Route path="/ico" exact component={IcoPage} />
     </div>
