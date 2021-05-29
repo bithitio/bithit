57ea8473371c113719a9df5e9c5d8868617a4489
bithitio <84578059+bithitio@users.noreply.github.com>
2021-05-29 07:56:55 +0700

    Update Bitcommunity.js

--- src/components/Bitcommunity.js
+++ src/components/Bitcommunity.js
@@ -12,16 +12,18 @@ const Bitcommunity = () => {
             </div>
             <div class="col-text">
                 <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially 
+                We provide a process of buying and selling products from smart contract base auctions.     
                 </p>
                 <p>
-                unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                Create first global risk-free blockchain based smart contract for bidding system.
                 </p>
                 <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
-                including versions of Lorem Ipsum.
+                We want to drive up access to financial services for unbanked entrepreneurs by converting assets into capital using NFT.    
                 </p>
-            </div>
+                <p>
+                Data from entrepreneurs using the platform will receive credit score from their customers, this is part of credit rating mechanism for merchant shops and provide easier access to capital.
+                </p>
+                    </div>
             <div class="col-text fullcol">
                 <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining 
