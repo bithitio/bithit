f1c9ff207e0dba297de57c26978b11fc62c57666
BitHit <84578059+bithitio@users.noreply.github.com>
2021-06-22 03:50:52 +0700

    Add files via upload

---
+++ src/pages/BuyForm.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+import bithitLogo from './bithit-logo.png'
+import bnbLogo from './bnb-logo.png'
+
+class BuyForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      output: '0'
+    }
+  }
+
+  render() {
+    return (
+      <form className="mb-3" onSubmit={(event) => {
+          event.preventDefault()
+          let etherAmount
+          etherAmount = this.input.value.toString()
+          etherAmount = window.web3.utils.toWei(etherAmount, 'Ether')
+          this.props.buyTokens(etherAmount)
+        }}>
+        <div>
+          <label className="float-left"><b>Input</b></label>
+          <span className="float-right text-muted">
+            Balance: {window.web3.utils.fromWei(this.props.ethBalance, 'Ether')}
+          </span>
+        </div>
+        <div className="input-group mb-4">
+          <input
+            type="text"
+            onChange={(event) => {
+              const etherAmount = this.input.value.toString()
+              this.setState({
+                output: etherAmount * 390000
+              })
+            }}
+            ref={(input) => { this.input = input }}
+            className="form-control form-control-lg"
+            placeholder="0"
+            required />
+          <div className="input-group-append">
+            <div className="input-group-text">
+              <img src={bnbLogo} height='32' alt=""/>
+              &nbsp;&nbsp;&nbsp; BNB
+            </div>
+          </div>
+        </div>
+        <div>
+          <label className="float-left"><b>Output</b></label>
+          <span className="float-right text-muted">
+            Balance: {window.web3.utils.fromWei(this.props.tokenBalance, 'Ether')}
+          </span>
+        </div>
+        <div className="input-group mb-2">
+          <input
+            type="text"
+            className="form-control form-control-lg"
+            placeholder="0"
+            value={this.state.output}
+            disabled
+          />
+          <div className="input-group-append">
+            <div className="input-group-text">
+              <img src={bithitLogo} height='32' alt=""/>
+              &nbsp; BITHIT
+            </div>
+          </div>
+        </div>
+        <div className="mb-5">
+          <span className="float-left text-muted">Exchange Rate</span>
+          <span className="float-right text-muted">1 BNB = 390000 BITHIT</span>
+        </div>
+        <button type="submit" className="btn btn-primary btn-block btn-lg">Buy!</button>
+      </form>
+    );
+  }
+}
+
+export default BuyForm;
---
+++ src/pages/Main.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import BuyForm from './BuyForm'
+
+class Main extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentForm: 'buy'
+    }
+  }
+
+  render() {
+    let content
+    if(this.state.currentForm === 'buy') {
+      content = <BuyForm
+        ethBalance={this.props.ethBalance}
+        tokenBalance={this.props.tokenBalance}
+        buyTokens={this.props.buyTokens}
+      />
+    } else {
+      content = <BuyForm
+        ethBalance={this.props.ethBalance}
+        tokenBalance={this.props.tokenBalance}
+        buyTokens={this.props.buyTokens}
+      />
+    }
+
+    return (
+      <div id="content" className="mt-3">
+        <br></br>
+        <br></br>
+        <div className="card mb-4" >
+          <div className="card-body">
+            {content}
+          </div>
+        </div>
+        <br></br>        
+      <center><h6>You Address: {this.props.account}</h6></center>
+		</div>
+      
+    );
+  }
+}
+
+export default Main;
