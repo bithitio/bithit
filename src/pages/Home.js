fc3a31bf9f8e837657c2f8813c5015c497b3e1a3
BitHit <84578059+bithitio@users.noreply.github.com>
2021-06-21 21:18:56 +0700

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
-
+import Header from '../Header';
+import Footer from '../Footer';
 import Bitrounds from "../components/Bitrounds";
 import Bitcommunity from "../components/Bitcommunity";
 import Bitonapp from "../components/Bitonapp";
@@ -15,20 +16,19 @@ const HomePage = () => {
 
     return(
         <div>
+            <Header />
             <div data-aos="fade-up">
-                <div class="container">
-                    
+                <div class="container">   
                 </div>
-
                 <div class="bit-cover-bnc"><img src={Bitbinance} alt="binance" /></div>
             </div>
-
             <div data-aos="fade-up"><Bitrounds /></div>
             <div data-aos="fade-up"><Bitcommunity /></div>
             <div data-aos="fade-up"><Bitonapp /></div>
             <div data-aos="fade-up"><Bittoken /></div>
             <div data-aos="fade-up"><Bitexchangelist /></div>
             <div><Bitroadmap /></div>
+            <Footer />
         </div>
     )
 };
--- src/pages/Howtobuy.js
+++ src/pages/Howtobuy.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Header from '../Header';
+import Footer from '../Footer';
 
 const Howtobuy = () => {
     return(
+        <div>
+        <Header />
         <h1 class="h1-header">Coming Soon</h1>
+        <br></br>
+        <Footer />
+        </div>
     )
 };
 
